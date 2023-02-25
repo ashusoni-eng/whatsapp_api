5857d0a344e5d7e222d6e771a9b52884ef78974c
Ashish Soni <ashishsoni.mp07@gmail.com>
2023-02-25 21:29:11 +0530

    new update

--- index.js
+++ index.js
@@ -1,69 +1,90 @@
-const express= require("express");
-const body_parser= require("body-parser");
-const axios= require("axios");
+const express=require("express");
+const body_parser=require("body-parser");
+const axios=require("axios");
+const fs=require("fs");
+
 require('dotenv').config();
 
-const app= express().use(body_parser.json());
+const app=express().use(body_parser.json());
 
-const api_token=process.env.API_TOKEN;
-const mytoken=process.env.MYTOKEN;
+const token=process.env.TOKEN;
+const mytoken=process.env.MYTOKEN;//prasath_token
 
-app.listen(process.env.PORT,()=>{
-    console.log("Wehbhook is listeneing....");
+app.get('/logs', (req, res) => {
+  const logs = fs.readFileSync('/var/log/render.log', 'utf8');
+  res.send(logs);
 });
 
+app.listen(process.env.PORT,()=>{
+    console.log("webhook is listening");
+});
+
+//to verify the callback url from dashboard side - cloud api side
 app.get("/webhook",(req,res)=>{
-    let mode=res.query["hub.mode"];
-    let challenge=res.query["hub.challenge"];
-    let token=res.query["hub.verify_toke"];
+   let mode=req.query["hub.mode"];
+   let challange=req.query["hub.challenge"];
+   let token=req.query["hub.verify_token"];
 
 
     if(mode && token){
 
         if(mode==="subscribe" && token===mytoken){
-            res.status(200).send(challenge);        
+            res.status(200).send(challange);
         }else{
             res.status(403);
         }
+
     }
+
 });
 
-app.post("/webhook",(req,res)=>{
-    let body_param= req.body;
+app.post("/webhook",(req,res)=>{ //i want some 
+
+    let body_param=req.body;
+
     console.log(JSON.stringify(body_param,null,2));
 
     if(body_param.object){
-        if(body_param.entry &&
-            body_param.entry[0].changes &&
-            body_param.entry[0].changes.value.message &&
-            body_param.entry[0].changes.value.message[0]
+        console.log("inside body param");
+        if(body_param.entry && 
+            body_param.entry[0].changes && 
+            body_param.entry[0].changes[0].value.messages && 
+            body_param.entry[0].changes[0].value.messages[0]  
             ){
-                let phone_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
-                let from=body_param.entry[0].changes[0].value.message[0].from;
-                let msg_body=body_param.entry[0].changes[0].value.message[0].text.body;
+               let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
+               let from = body_param.entry[0].changes[0].value.messages[0].from; 
+               let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
-                axios({
-                    method:"POST",
-                    url:"https://graph.facebook.com/v15.0/"+phone_no_id+"/messages?access_token="+api_token,
-                    data:{
-                        messaging_product:"whatsapp",
-                        to:from,
-                        text:{
-                            body:"Hi.... I am Ashish"
-                        }
-                    },
-                    headers:{
-                        "Content-Type":"application/json"
-                    }
-                });
+               console.log("phone number "+phon_no_id);
+               console.log("from "+from);
+               console.log("boady param "+msg_body);
 
-                res.sendStatus(200);
+               axios({
+                   method:"POST",
+                   url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
+                   data:{
+                       messaging_product:"whatsapp",
+                       to:from,
+                       text:{
+//                            body:"Hi.. I'm Ashish, your message is "+msg_body
+                                body:"Hi.. I'm Ashish, We will contact you as soon as possible. Or you can call us on 07512437375 "
+                       }
+                   },
+                   headers:{
+                       "Content-Type":"application/json"
+                   }
+
+               });
+
+               res.sendStatus(200);
             }else{
                 res.sendStatus(404);
             }
+
     }
+
 });
 
 app.get("/",(req,res)=>{
-    res.status(200).send("This is Webhook Setup");
-})
+    res.status(200).send("hello this is webhook setup");
+});
