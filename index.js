c1157a122f1ad5344abb0445257f64dc76b76c7f
Ashish Soni <ashishsoni.mp07@gmail.com>
2023-02-26 11:56:44 +0530

    auto reply config and clean

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(cors()); // Enable CORS for all routes
 let latestData = null;
 
 const token = process.env.TOKEN;
-const mytoken = process.env.MYTOKEN; //prasath_token
+const mytoken = process.env.MYTOKEN;
+
 // Store all webhook data in memory
 let allData = {
   messages: [],
@@ -39,7 +40,6 @@ app.get("/webhook", (req, res) => {
 });
 
 app.post("/webhook", (req, res) => {
-  //i want some
 
   let body_param = req.body;
 
@@ -50,48 +50,6 @@ app.post("/webhook", (req, res) => {
     app.emit("webhook-data-update");
   }
 
-  // console.log(JSON.stringify(body_param,null,2));
-  // webData= body_param;
-
-  /*use to send auto reply when someone send message*/
-  //     if(body_param.object){
-  //         console.log("inside body param");
-  //         if(body_param.entry &&
-  //             body_param.entry[0].changes &&
-  //             body_param.entry[0].changes[0].value.messages &&
-  //             body_param.entry[0].changes[0].value.messages[0]
-  //             ){
-  //                let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
-  //                let from = body_param.entry[0].changes[0].value.messages[0].from;
-  //                let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
-
-                //  console.log("phone number "+phon_no_id);
-                //  console.log("from "+from);
-                //  console.log("boady param "+msg_body);
-
-  //                axios({
-  //                    method:"POST",
-  //                    url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
-  //                    data:{
-  //                        messaging_product:"whatsapp",
-  //                        to:from,
-  //                        text:{
-  // //                            body:"Hi.. I'm Ashish, your message is "+msg_body
-  //                                 body:"Hi.. I'm Ashish, We will contact you as soon as possible. Or you can call us on 07512437375 "
-  //                        }
-  //                    },
-  //                    headers:{
-  //                        "Content-Type":"application/json"
-  //                    }
-
-  //                });
-
-  //                res.sendStatus(200);
-  //             }else{
-  //                 res.sendStatus(404);
-  //             }
-
-  //     }
 
   if (body_param.object) {
     console.log("inside body param");
@@ -101,7 +59,7 @@ app.post("/webhook", (req, res) => {
       body_param.entry[0].changes[0].value.messages &&
       body_param.entry[0].changes[0].value.messages[0]
     ) {
-      // Store the latest data in memory
+
       // Store the latest data in memory
       const newData = {
         phone_no_id: body_param.entry[0].changes[0].value.metadata.phone_number_id,
@@ -111,13 +69,12 @@ app.post("/webhook", (req, res) => {
         display_no:body_param.entry[0].changes[0].value.metadata.display_phone_number
       };
       updateLatestData(newData);
-
       
       axios({
         phone_no_id: body_param.entry[0].changes[0].value.metadata.phone_number_id,
         from: body_param.entry[0].changes[0].value.messages[0].from,
         method: "POST",
-        url:"https://graph.facebook.com/v15.0/" +phon_no_id +"/messages?access_token="+token,
+        url:"https://graph.facebook.com/v15.0/" +phone_no_id +"/messages?access_token="+token,
         data: {
           messaging_product: "whatsapp",
           to: from,
@@ -130,17 +87,10 @@ app.post("/webhook", (req, res) => {
         },
       });
 
-      // Emit an event to indicate that the latestData variable has been updated
-      // app.emit("webhook-data-update");
-
-      res.sendStatus(200); //if remove comment than it should also removed
+      res.sendStatus(200); 
     }
   }
 });
-
-// app.get("/",(req,res)=>{
-//     res.status(200).send(webData);
-// });
 
 app.delete("/webhook-data", (req, res) => {
   allData.messages = [];
@@ -167,5 +117,3 @@ app.get("/webhook-data", (req, res) => {
     app.off("webhook-data-update", listener);
   });
 });
-
-// res.status(200).send(webData);
