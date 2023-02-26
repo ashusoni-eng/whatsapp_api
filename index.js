feacc3631879a5510b2d402fa4177cc486d5fa4f
Ashish Soni <ashishsoni.mp07@gmail.com>
2023-02-26 09:28:24 +0530

    comment out auto reply code

--- index.js
+++ index.js
@@ -42,44 +42,44 @@ app.post("/webhook",(req,res)=>{ //i want some
     // console.log(JSON.stringify(body_param,null,2));
     webData= JSON.stringify(body_param,null,2);
 
-    if(body_param.object){
-        console.log("inside body param");
-        if(body_param.entry && 
-            body_param.entry[0].changes && 
-            body_param.entry[0].changes[0].value.messages && 
-            body_param.entry[0].changes[0].value.messages[0]  
-            ){
-               let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
-               let from = body_param.entry[0].changes[0].value.messages[0].from; 
-               let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+//     if(body_param.object){
+//         console.log("inside body param");
+//         if(body_param.entry && 
+//             body_param.entry[0].changes && 
+//             body_param.entry[0].changes[0].value.messages && 
+//             body_param.entry[0].changes[0].value.messages[0]  
+//             ){
+//                let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
+//                let from = body_param.entry[0].changes[0].value.messages[0].from; 
+//                let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
-               console.log("phone number "+phon_no_id);
-               console.log("from "+from);
-               console.log("boady param "+msg_body);
+//                console.log("phone number "+phon_no_id);
+//                console.log("from "+from);
+//                console.log("boady param "+msg_body);
 
-               axios({
-                   method:"POST",
-                   url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
-                   data:{
-                       messaging_product:"whatsapp",
-                       to:from,
-                       text:{
-//                            body:"Hi.. I'm Ashish, your message is "+msg_body
-                                body:"Hi.. I'm Ashish, We will contact you as soon as possible. Or you can call us on 07512437375 "
-                       }
-                   },
-                   headers:{
-                       "Content-Type":"application/json"
-                   }
+//                axios({
+//                    method:"POST",
+//                    url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
+//                    data:{
+//                        messaging_product:"whatsapp",
+//                        to:from,
+//                        text:{
+// //                            body:"Hi.. I'm Ashish, your message is "+msg_body
+//                                 body:"Hi.. I'm Ashish, We will contact you as soon as possible. Or you can call us on 07512437375 "
+//                        }
+//                    },
+//                    headers:{
+//                        "Content-Type":"application/json"
+//                    }
 
-               });
+//                });
 
-               res.sendStatus(200);
-            }else{
-                res.sendStatus(404);
-            }
+//                res.sendStatus(200);
+//             }else{
+//                 res.sendStatus(404);
+//             }
 
-    }
+//     }
 
 });
 
